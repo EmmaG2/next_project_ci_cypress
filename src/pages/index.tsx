9d935d7c64353b136eb5f136fe91f3f3015b06e0
EmmaG2 <itsmeemmadev@gmail.com>
2023-05-01 13:29:08 -0600

    [shop] layout created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { ShopLayout } from '@/components/layouts'
+import { Typography } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <h1>Hola mundo</h1>
+  return (
+    <ShopLayout
+      title='Tesla-Shop - Home'
+      pageDescription='Encuentra los mejores productos de tesla aqui'
+    >
+      <Typography
+        variant='h1'
+        component='h1'
+      >
+        Home
+      </Typography>
+      <Typography
+        variant='h2'
+        component='h2'
+        sx={{
+          mb: 1,
+        }}
+      >
+        Todos los productos
+      </Typography>
+    </ShopLayout>
+  )
 }
