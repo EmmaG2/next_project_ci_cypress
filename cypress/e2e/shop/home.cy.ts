809c61ade09118de4aacc0921f3d14b9e340f655
EmmaG2 <itsmeemmadev@gmail.com>
2023-05-05 09:47:35 -0600

    [tests]: home tests refactor

--- cypress/e2e/shop/home.cy.ts
+++ cypress/e2e/shop/home.cy.ts
@@ -1,13 +1,13 @@
-describe('Tests for the home ref', () => {
-  it('should has a correct title', () => {
+describe('should render the home page', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000')
-    
+  })
+
+  it('should has a correct title', () => {
     cy.get('h1').contains('Home')
   })
 
   it('should has a correct subtitle', () => {
-    cy.visit('http://localhost:3000')
-
     cy.get('h2').contains('Todos los productos')
   })
 })
